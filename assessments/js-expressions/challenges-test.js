b5af9377e86cddda3cbc5db0e9544f1170db3703
Brooks Patton <brooks.patton@galvanize.com>
2017-01-19 15:38:35 -0700

    starting to build out what we want for assessments

---
+++ assessments/js-expressions/challenges-test.js
@@ -0,0 +1,4 @@
+// Challenge 1
+// Create a variable called bikes, store the number 20 in bikes
+
+// [] Was the variable bikes created and was it assigned the **number** 20?
---
+++ assessments/js-expressions/challenges.js
@@ -0,0 +1,7 @@
+// Standard - ???
+// Sub Standard - ???
+// Success Criteria - ???
+
+// Challenge 1
+// Create a variable called bikes, store the number 20 in bikes
+var bikes = 20
