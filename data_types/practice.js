042e89035b7669a95f311812e54b6a9454724c38
a25patel <aanaletap2510@gmail.com>
2018-01-11 16:18:56 -0700

    deleted all repititious and focused on merging/organizing problems that were helpful

---
+++ data_types/practice.js
@@ -0,0 +1,35 @@
+// Swamp Things
+
+var croc
+  20 fish a month
+  1 snake a week
+  1 bird a week
+
+var snake
+  1 bird a week
+
+var bird
+  2 fish a day
+
+var fish
+  Herbivores
+
+* If not enough food, remove from ecosystem. Extinct.
+
+var fish
+  2 fish -> 100 fish every month
+
+var birds
+  2 birds -> 5 birds every month
+
+var snake
+  2 snakes -> 10 snakes twice a year
+  March / September
+
+var croc
+  2 crocs -> 5 crocs once a year
+  June
+
+*** how many fish at end of March and end of Sept
+*** # snakes in 1 year?
+*** birds extinct in 500 years? how many months until they do?
